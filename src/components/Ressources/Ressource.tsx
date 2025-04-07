0fd4abbed6959c5c1b922bcb431ffd882b3d730a
Thomas Leconte <thomasl.dw20@gmail.com>
2025-04-08 01:52:39 +0200

    test Ressources version 2

---
+++ src/components/Ressources/Ressource.tsx
@@ -0,0 +1,10 @@
+import styles from './Ressource.module.css';
+
+export default function Ressource() {
+    return (
+      <section className=" bg-pink-700 dark:bg-pink-500 size-16 text-center p-[20px]" id={styles.ressources}>
+        Ressources
+      </section>
+    );
+  }
+  
--- src/components/Ressources/Ressources.tsx
+++ src/components/Ressources/Ressources.tsx
@@ -1,9 +1,11 @@
+import Ressource from './Ressource';
 import styles from './Ressources.module.css';
 
 export default function Ressources() {
     return (
       <section className="bg-sky-900 w-full text-center p-[20px]" id={styles.ressources}>
         Ressources
+        <Ressource />
       </section>
     );
   }
