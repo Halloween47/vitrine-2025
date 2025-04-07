fb65565c754de50c6e4bc16915f39dee3671bebc
Thomas Leconte <thomasl.dw20@gmail.com>
2025-04-08 00:12:46 +0200

    test probleme deploy vercel version 5

--- src/app/testpage.tsx
+++ src/app/testpage.tsx
@@ -1,18 +1,11 @@
 // import Image from "next/image";
 import styles from "./page.module.css";
-import HeroSection from "@/components/testHeroSection";
-import Projets from "@/components/testProjets";
-import Offres from "@/components/testOffres";
-import Contact from "@/components/testContact";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <HeroSection />
-        <Projets />
-        <Offres />
-        <Contact />
+        page test
       </main>
     </div>
   );
