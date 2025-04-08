69396820ae386777169e119fd661f0f682bf8389
Thomas Leconte <thomasl.dw20@gmail.com>
2025-04-08 02:11:50 +0200

    création zone projets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Contact from "@/components/Contact/Contact";
 import styles from "./page.module.css";
 import Ressources from "@/components/Ressources/Ressources";
+import Projets from "@/components/Projets/Projets";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Hero />
+        <Projets />
         <Ressources />
         <Contact />
       </main>
---
+++ src/components/Projets/Projets.tsx
@@ -0,0 +1,23 @@
+export default function Projets() {
+    return (
+      <section className="
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        min-h-50 
+        bg-[#49111C]
+        text-white 
+        text-center
+        rounded-[5px]
+        gap-5
+        w-full
+        h-[20vh]
+      ">
+        <h2 className="text-1xl font-bold leading-tight uppercase">
+            Ici Projets
+        </h2>
+      </section>
+    );
+  }
+  
