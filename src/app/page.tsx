a4916dc190887cddee47b47165bdde184f3d697c
Thomas Leconte <thomasl.dw20@gmail.com>
2025-04-18 23:43:28 +0200

    MOBILE Home (services) : test visuel v7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 // import Image from "next/image";
-import Hero from "@/components/Hero";
+// import Hero from "@/components/Hero";
 // import Hero1 from "@/components/tests/Hero1";
 // import Hero3 from "@/components/tests/Hero3";
 // import ServicesButtonGroup from "@/components/Services/ServicesButtonGroup";
 // import ServicesCards from "@/components/Services/ServicesCard";
-import Contact from "@/components/Contact/Contact";
+// import Contact from "@/components/Contact/Contact";
 import styles from "./page.module.css";
-import Ressources from "@/components/Ressources/Ressources";
-import Projets from "@/components/Projets/Projets";
-import Tarifs from "@/components/Tarifs/Tarifs";
+// import Ressources from "@/components/Ressources/Ressources";
+// import Projets from "@/components/Projets/Projets";
+// import Tarifs from "@/components/Tarifs/Tarifs";
 import BetaBanner from "@/components/Beta/BetaBanner";
 import HeroDemo from "@/components/Demo/HeroDemo";
 import Dispo from "@/components/Beta/Dispo";
-import Services from "@/components/Services/Services";
-import TestServicesCard from "@/components/Services/TestServiceCard";
+// import Services from "@/components/Services/Services";
+// import TestServicesCard from "@/components/Services/TestServiceCard";
 import ServicesCarousel from "@/components/Services/ServicesCarousel";
 
 
@@ -26,18 +26,18 @@ export default function Home() {
         <HeroDemo />
         {/* <Hero1 /> */}
         {/* <Hero3 /> */}
-        <TestServicesCard />
+        {/* <TestServicesCard /> */}
         <ServicesCarousel />
         {/* <ServicesButtonGroup /> */}
         {/* <ServicesCards /> */}
-        <Services />
-        <Hero />
-        <div className="home-content flex flex-wrap justify-between mt-5 gap-5">
+        {/* <Services /> */}
+        {/* <Hero /> */}
+        {/* <div className="home-content flex flex-wrap justify-between mt-5 gap-5">
           <Projets />
           <Tarifs />
           <Ressources />
           <Contact />
-        </div>
+        </div> */}
       </main>
     </div>
   );
