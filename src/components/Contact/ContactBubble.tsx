8ab35b9578cb71aa87fae81342a1682f6a9437b5
Thomas Leconte <thomasl.dw20@gmail.com>
2025-04-28 00:45:25 +0200

    MOBILE Home contact : modif position

--- src/components/Contact/ContactBubble.tsx
+++ src/components/Contact/ContactBubble.tsx
@@ -40,7 +40,7 @@ export default function ContactBubble() {
   ]
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-19 right-6 z-50">
       <div className="relative">
         {/* <button */}
         <Button
